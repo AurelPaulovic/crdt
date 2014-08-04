a498aba0e62dea45483c2bf4058d0e9ee74f3392
Aurel Paulovic <aurel.paulovic@gmail.com>
2014-08-04 16:30:32 +0200

    NamedReplica should be serializable

--- src/main/scala/com/aurelpaulovic/crdt/replica/NamedReplica.scala
+++ src/main/scala/com/aurelpaulovic/crdt/replica/NamedReplica.scala
@@ -16,7 +16,7 @@
 
 package com.aurelpaulovic.crdt.replica
 
-class NamedReplica(private val name: String) extends Replica {
+class NamedReplica(private val name: String) extends Replica with Serializable {
   override def equals(other: Any): Boolean = other match {
     case (that: NamedReplica) => that.isInstanceOf[NamedReplica] && name == that.name
     case _ => false
